7f684da4d292d43792f57156168e59a52746fdb7
aprexter <sachin9519059871@gmail.com>
2026-08-22 18:22:58 +0530

    remove some

--- src/main/java/com/aprexter/googledocs/security/UserDetail.java
+++ src/main/java/com/aprexter/googledocs/security/UserDetail.java
@@ -25,20 +25,6 @@ public class UserDetail implements UserDetails {
         return "";
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
 
     @Override
     public boolean isEnabled() {
